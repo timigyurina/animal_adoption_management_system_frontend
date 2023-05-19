cdce83a9f6146b24ef4e0b18a0bbc618b7b182e7
timigyurina <timi.gyurina@gmail.com>
2023-05-19 11:03:58 +0200

    move and create elements for showing content (later)

---
+++ src/Components/Content/adoptionApplication/AdoptionApplicationCards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdoptionApplicationCards = ({adoptionApplications}) => {
+  return (
+    <div>AdoptionApplicationCards</div>
+  )
+}
+
+export default AdoptionApplicationCards
---
+++ src/Components/Content/image/ImageCards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImageCards = ({images}) => {
+  return (
+    <div>ImageCards</div>
+  )
+}
+
+export default ImageCards
