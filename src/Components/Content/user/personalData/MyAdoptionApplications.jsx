224635b3d6bcdd6d71f28422b1aa1d14722dd48a
timigyurina <timi.gyurina@gmail.com>
2023-06-19 09:18:47 +0200

    create MyAdoptionApplications

--- src/Components/Content/user/personalData/MyAdoptionApplications.jsx
+++ src/Components/Content/user/personalData/MyAdoptionApplications.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useFetch } from "../../../../hooks/useFetch";
+
+import SnackbarWithMessage from "../../../SharedElements/SnackbarWithMessage";
+import Loader from "../../../SharedElements/Loader";
+import AdoptionApplicationCard from "../../adoptionApplication/AdoptionApplicationCard";
+import { Box } from "@mui/material";
+
+const cardBoxStyles = {
+  minWidth: "250px",
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  justifyContent: "space-between",
+  alignItems: "center",
+  gap: "1em",
+};
+
+// this component gets a User's own applications, not all of them
 
 const MyAdoptionApplications = () => {
-  return (
-    <div>MyAdoptionApplications</div>
-  )
-}
+  const { loading, error, clearError, sendRequest } = useFetch();
+  const [adoptionApplicationsOfUser, setAdoptionApplicationsOfUser] = useState(
+    []
+  );
 
-export default MyAdoptionApplications
+  useEffect(() => {
+    const url = `${process.env.REACT_APP_BACKEND_URL}/api/user/adoptionApplication`;
+    const getAdoptionApplicationsOfUser = async () => {
+      try {
+        const responseData = await sendRequest(true, url);
+        setAdoptionApplicationsOfUser(responseData);
+        return;
+      } catch (err) {}
+    };
+    getAdoptionApplicationsOfUser();
+  }, [sendRequest]);
+
+  return (
+    <>
+      {error && (
+        <SnackbarWithMessage
+          message={error}
+          severity="error"
+          opened={error !== null}
+          closed={clearError}
+        />
+      )}
+      {loading ? (
+        <Loader />
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            gap: "1em",
+            minWidth: 500,
+            m: 1,
+            p: 2,
+          }}
+        >
+          {adoptionApplicationsOfUser.map((adoptionApplication) => (
+            <AdoptionApplicationCard
+              key={adoptionApplication.id}
+              adoptionApplication={adoptionApplication}
+              cardBoxStyles={cardBoxStyles}
+            />
+          ))}
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default MyAdoptionApplications;
