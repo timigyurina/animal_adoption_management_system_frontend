cbb985eb8b6d2293b72c291439bd1d0172d5ff31
timigyurina <timi.gyurina@gmail.com>
2023-06-19 09:24:31 +0200

    Add content for AdoptionApplications and -Cards
    Create filters in AdoptionApplications, use filters and do the fetching in AdoptionApplicationCards.

--- src/Components/Content/adoptionApplication/AdoptionApplicationCards.jsx
+++ src/Components/Content/adoptionApplication/AdoptionApplicationCards.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useFetch } from "../../../hooks/useFetch";
 
-const AdoptionApplicationCards = ({adoptionApplications}) => {
+import AdoptionApplicationCard from "./AdoptionApplicationCard";
+import Loader from "../../SharedElements/Loader";
+import SnackbarWithMessage from "../../SharedElements/SnackbarWithMessage";
+import CustomPagination from "../../SharedElements/CustomPagination";
+import { Box } from "@mui/material";
+
+const cardBoxStyles = {
+  minWidth: "250px",
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  justifyContent: "space-between",
+  alignItems: "center",
+  gap: "1em",
+};
+
+const AdoptionApplicationCards = ({ filters }) => {
+  const { loading, error, clearError, sendRequest } = useFetch();
+  const [adoptionApplications, setAdoptionApplications] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [numberOfPages, setNumberOfPages] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
+
+  useEffect(() => {
+    const url = `${
+      process.env.REACT_APP_BACKEND_URL
+    }/api/adoptionApplication/pageAndFilter?pagesize=${pageSize}&pageNumber=${currentPage}&animalName=${
+      filters.animalName
+    }&applierName=${filters.applierName}&status=${
+      filters.applicationStatus
+    }&dateBefore=${
+      filters.dateBefore === null ? "" : filters.dateBefore
+    }&dateAfter=${filters.dateAfter === null ? "" : filters.dateAfter}`;
+    const getAdoptionApplications = async () => {
+      try {
+        const responseData = await sendRequest(true, url);
+        setAdoptionApplications(responseData.items);
+        setNumberOfPages(responseData.numberOfPages);
+        setTotalCount(responseData.totalCount);
+        return;
+      } catch (err) {}
+    };
+    getAdoptionApplications();
+  }, [filters, currentPage, pageSize, sendRequest]);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [filters]);
+
+  const handleChangeItemsPerPage = (event) => {
+    setPageSize(parseInt(event.target.value, 10));
+    setCurrentPage(1);
+  };
+
   return (
-    <div>AdoptionApplicationCards</div>
-  )
-}
+    <>
+      {error && (
+        <SnackbarWithMessage
+          message={error}
+          severity="error"
+          opened={error !== null}
+          closed={clearError}
+        />
+      )}
 
-export default AdoptionApplicationCards
+      <CustomPagination
+        pageSize={pageSize}
+        currentPage={currentPage}
+        pageCount={numberOfPages}
+        onPageChange={(event, value) => {
+          setCurrentPage(value);
+        }}
+        onCountChange={handleChangeItemsPerPage}
+        totalCount={totalCount}
+      />
+
+      {loading ? (
+        <Loader />
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            gap: "1em",
+            minWidth: 500,
+            m: 2,
+            p: 1,
+          }}
+        >
+          {adoptionApplications.map((adoptionApplication) => (
+            <AdoptionApplicationCard
+              key={adoptionApplication.id}
+              adoptionApplication={adoptionApplication}
+              cardBoxStyles={cardBoxStyles}
+              adminMode
+            />
+          ))}
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default AdoptionApplicationCards;
--- src/Components/Content/adoptionApplication/AdoptionApplications.jsx
+++ src/Components/Content/adoptionApplication/AdoptionApplications.jsx
@@ -1,9 +1,179 @@
-import React from 'react'
+import { useState, useContext } from "react";
+import { useFetch } from "../../../hooks/useFetch";
+import { EnumContext } from "../../../context/EnumContext";
+
+import AdoptionApplicationCards from "./AdoptionApplicationCards";
+import SelectValues from "../../SharedElements/SelectValues";
+import {
+  Box,
+  TextField,
+  FormControl,
+  Typography,
+  Button,
+  CircularProgress,
+  FormHelperText,
+} from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+
+// component for setting filters for AdoptionApplications and then passing those down to AdoptionApplicationCards
+// only admins can access the route of this component
 
 const AdoptionApplications = () => {
-  return (
-    <div>AdoptionApplications</div>
-  )
-}
+  const { loading } = useFetch();
+  const { applicationStatus } = useContext(EnumContext).enums;
 
-export default AdoptionApplications
+  const emptyFilters = {
+    animalName: "",
+    applierName: "",
+    applicationStatus: "",
+    dateAfter: null,
+    dateBefore: null,
+  };
+  const [filters, setFilters] = useState(emptyFilters);
+  const [filtersAreOpen, setFiltersAreOpen] = useState(true);
+
+  const handleFiltersChange = (e) => {
+    const { name, value } = e.target;
+    setFilters((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      <Box sx={{ textAlign: "center", m: 2 }}>
+        <Button
+          variant="contained"
+          onClick={() => setFiltersAreOpen(!filtersAreOpen)}
+        >
+          {filtersAreOpen ? "Close filters" : "Open filters"}
+        </Button>
+      </Box>
+
+      {filtersAreOpen && (
+        <FormControl
+          sx={{ m: 1, display: "flex", alignItems: "center" }}
+          component="fieldset"
+          variant="standard"
+        >
+          <Typography variant="h5" sx={{ textAlign: "center" }}>
+            AdoptionApplication filters
+          </Typography>
+
+          <Box
+            mt={1}
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-around",
+              alignItems: "center",
+              width: "100%",
+              gap: 1,
+            }}
+          >
+            {loading ? (
+              <CircularProgress />
+            ) : (
+              <SelectValues
+                choosableItems={applicationStatus}
+                value={filters.applicationStatus}
+                onChange={handleFiltersChange}
+                label="Application status"
+                selectName="applicationStatus"
+                small
+                withDefault
+              />
+            )}
+
+            <Box
+              my={1}
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-around",
+                alignItems: "center",
+                width: "100%",
+                gap: 1,
+              }}
+            >
+              <TextField
+                label="Name of Animal"
+                name="animalName"
+                value={filters.animalName}
+                onChange={handleFiltersChange}
+                variant="outlined"
+                placeholder="Search for Animal..."
+                size="small"
+              />
+              <TextField
+                label="Name of Applier"
+                name="applierName"
+                value={filters.applierName}
+                onChange={handleFiltersChange}
+                variant="outlined"
+                placeholder="Search for Applier..."
+                size="small"
+              />
+
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Date after"
+                  inputFormat="YYYY/MM/DD"
+                  defaultValue={dayjs("2022-04-17")}
+                  value={filters.dateAfter}
+                  onChange={(newValue) =>
+                    setFilters({ ...filters, dateAfter: newValue })
+                  }
+                  slotProps={{ textField: { variant: "outlined" } }}
+                />
+                <DatePicker
+                  label="Date before"
+                  inputFormat="YYYY/MM/DD"
+                  value={filters.dateBefore}
+                  onChange={(newValue) =>
+                    setFilters({ ...filters, dateBefore: newValue })
+                  }
+                  slotProps={{ textField: { variant: "outlined" } }}
+                />
+                {filters.dateAfter !== null &&
+                  filters.dateBefore !== null &&
+                  filters.dateAfter > filters.dateBefore && (
+                    <FormHelperText error>
+                      Date before cannot be earlier than date after
+                    </FormHelperText>
+                  )}
+              </LocalizationProvider>
+            </Box>
+
+            <Box
+              my={1}
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-around",
+                width: "70%",
+              }}
+            >
+              <Button
+                variant="outlined"
+                color="ternary"
+                onClick={() => setFilters(emptyFilters)}
+              >
+                Clear all filters
+              </Button>
+            </Box>
+          </Box>
+        </FormControl>
+      )}
+
+      <AdoptionApplicationCards filters={filters} />
+    </>
+  );
+};
+
+export default AdoptionApplications;
